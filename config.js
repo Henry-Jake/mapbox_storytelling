ef24741d1eba38c2a9e45284c0b9d93ab6be94f7
Henry-Jake <jfyjhenry101@gmail.com>
2025-05-03 14:29:39 -0400

    changed title of As I Lay Dying to Bold

--- config.js
+++ config.js
@@ -68,8 +68,8 @@ var config = {
             id: 'chapter-one',
             alignment: 'right',
             hidden: false,
-            title: ' <em> As I Lay Dying <en> By Willaim Faulkner',
-            image: './As_I_Lay_Dying_Cover.png',
+            title: ' <em> As I Lay Dying <em> By Willaim Faulkner',
+            image: '/As_I_Lay_Dying_Cover.png',
             description: 'As I Lay Dying',
             location: {
                 center: [-83.5573651, 32.5573651], 
