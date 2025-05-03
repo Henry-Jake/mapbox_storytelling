42ca2ebc8ee97c73b4871205d95ef91d20e95cef
Henry-Jake <jfyjhenry101@gmail.com>
2025-05-03 19:54:54 -0400

    changed Nettie location

--- config.js
+++ config.js
@@ -178,7 +178,7 @@ var config = {
             image: './assets/christian_nigeria.png',
             description: 'On the other hand, after Nettie is kicked out, she and Celie\’s children (Olivia and Adam) make their way to New York where they are taken in by a couple of missionaries and spend much of their life living with a native tribe (The Olinka) in West Africa. However, before settling in West Africa for a time, she travels to places like London and Southampton in England, as well as Lisbon in Portugal, Dakar in Senegal, and Monrovia in Liberia. While she is able to travel to these locations with the help of the church, she is unsuccessful in her mission of helping the Olinka people re-locate as their land and homes get taken over and repurposed into rubber plantations. When church funds begin to run out, she is unsuccessful in convincing the church to help any further and is forced to dip into her own pocket to try and aid the Olinka. She returns to Georgia after this failure as she begins receiving letters from Celie and wants to reconnect with her sister and have her children meet their birth mother. ',
             location: {
-                center: [-17.64595608016324, 14.78768183660224], 
+                center: [-89.2678409578089,30.378878503582193], 
                 zoom: 5,
                 pitch: 60,
                 bearing: 0
